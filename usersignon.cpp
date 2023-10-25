20823db12e67df5c15bfecec1c6abe790bc9de6d
NuiRukki <117103328+NuiRukki@users.noreply.github.com>
2023-10-25 13:26:13 +0800

    Update usersignon.cpp

--- usersignon.cpp
+++ usersignon.cpp
@@ -1,14 +1,1 @@
-#include "usersignon.h"
-#include "ui_usersignon.h"
 
-UserSignOn::UserSignOn(QWidget *parent) :
-    QDialog(parent),
-    ui(new Ui::UserSignOn)
-{
-    ui->setupUi(this);
-}
-
-UserSignOn::~UserSignOn()
-{
-    delete ui;
-}
